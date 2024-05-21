4b894daf5cb05b8b56240d21915f47137eaeb076
Kuldeep Singh <986893ks@gmail.com>
2024-05-21 15:59:37 +0530

    Mota mota UI desgin krlia h

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,22 @@
+import { AiFillDelete } from "react-icons/ai";
+
 const Post = ({ post }) => {
   return (
-    <div className="card post-card" style={{ width: "18rem" }}>
+    <div className="card post-card" style={{ width: "30rem" }}>
       <div className="card-body">
-        <h5 className="card-title">{post.title}</h5>
+        <h5 className="card-title">
+          {post.title}
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            <AiFillDelete />
+          </span>
+        </h5>
         <p className="card-text">{post.body}</p>
-        {post.tags.map((tag) => {
-          <span className="badge text-bg-primary">{tag}</span>;
-        })}
+        {post.tags.map((tag) => (
+          <span className="badge text-bg-primary hashtag">{tag}</span>
+        ))}
+        <div className="alert alert-success reactions" role="alert">
+          This post has been reacted by {post.reactions} people
+        </div>
       </div>
     </div>
   );
