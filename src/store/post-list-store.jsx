ead343e1ed1543e7a29faabc9f64df6a0eefb3eb
Kuldeep Singh <986893ks@gmail.com>
2024-05-23 17:25:24 +0530

    Version1_SocialMeida

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -7,12 +7,42 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currPostList, action) => {
-  return currPostList;
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
 };
 const PostListProvider = ({ children }) => {
-  const [postList, dispatch] = useReducer(postListReducer, DEFAULT_POST_LIST);
-  const addPost = () => {};
-  const deletePost = () => {};
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        tags: tags,
+        userId: userId,
+      },
+    });
+  };
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
 
   return (
     <PostList.Provider value={{ postList, addPost, deletePost }}>
